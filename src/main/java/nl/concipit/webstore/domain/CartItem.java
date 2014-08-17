622ada5046d7907a16615fd8097bc7777f09f977
davidcoppens <david@davidcoppens.nl>
2014-08-17 13:07:35 +0200

    Added shopping cart domain items

---
+++ src/main/java/nl/concipit/webstore/domain/CartItem.java
@@ -0,0 +1,84 @@
+package nl.concipit.webstore.domain;
+
+import java.math.BigDecimal;
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+    private BigDecimal totalPrice;
+
+    public CartItem() {
+        // default constructor
+    }
+
+    public CartItem(Product product) {
+        this.product = product;
+        this.quantity = 1;
+        this.totalPrice = product.getUnitPrice();
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+        updateTotalPrice();
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+        updateTotalPrice();
+    }
+    
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+    
+
+    public void updateTotalPrice() {
+        totalPrice = this.product.getUnitPrice().multiply(
+                new BigDecimal(quantity));
+
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((product == null) ? 0 : product.hashCode());
+        result = prime * result + quantity;
+        result = prime * result
+                + ((totalPrice == null) ? 0 : totalPrice.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CartItem other = (CartItem) obj;
+        if (product == null) {
+            if (other.product != null)
+                return false;
+        } else if (!product.equals(other.product))
+            return false;
+        if (quantity != other.quantity)
+            return false;
+        if (totalPrice == null) {
+            if (other.totalPrice != null)
+                return false;
+        } else if (!totalPrice.equals(other.totalPrice))
+            return false;
+        return true;
+    }
+
+}
